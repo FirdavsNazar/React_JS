52c14a374fb66ff8aff9647d1de991d2961122d3
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-05-12 22:26:06 +0900

    React_state

--- React_tutorial/src/lesson_4/app.jsx
+++ React_tutorial/src/lesson_4/app.jsx
@@ -23,6 +23,7 @@ export default class App extends Component {
 return (
     <div>
         <button onClick={() => this.setState({count: this.state.count + 1})}> + </button>
+        <button onClick={() => this.setState({count: this.state.count - 1})}> - </button>
         <h1>{this.state.count}</h1>
         <h2>{this.state.title}</h2>
     <input onChange={(e) => this.setState({title : e.target.value})} type="text" placeholder='title' />
