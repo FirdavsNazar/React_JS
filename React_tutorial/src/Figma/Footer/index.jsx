3a2fe75e11dac9cc5a251c9751a72f97425ef3d8
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-05-23 20:02:45 +0900

    Figma

--- React_tutorial/src/Figma/Footer/index.jsx
+++ React_tutorial/src/Figma/Footer/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
-import { Container,Wrapper, BtnJoin, BtnEmail,FooterButton, FooterHeader,WrapperHeader } from './style'
+import logo from '../../assets/logo.png';
+import img1 from '../../assets/img1.png'
+import img2 from '../../assets/img2.png'
+import img3 from '../../assets/img3.png'
+import img4 from '../../assets/img4.png'
+import img5 from '../../assets/img5.png'
+import img6 from '../../assets/img6.png'
+import { Container,Wrapper,FooterImg, ContactNumber, ContactImges, ContactImg, ContactSpan, FooterContact, TypeSpan1, TypeHeader, FooterType, FooterImg2, FooterSpan3, FooterNetwork1, FooterNetwork3, FooterNetwork2, FooterSpan2, FooterSpan, FooterNetwork, Container2,FooterAdr, Wrapper2, BtnJoin, BtnEmail,FooterButton, FooterHeader,WrapperHeader, } from './style'
 
 const Footer = () => {
   return (
@@ -22,7 +29,78 @@ const Footer = () => {
 
           </WrapperHeader>
        </Wrapper>
+
+       <Container2>
+            <Wrapper2>
+                 <FooterAdr>
+                    <FooterNetwork>
+                        <FooterNetwork1>
+
+                              <FooterImg src={logo} alt="" />
+                             <FooterSpan>Explore</FooterSpan> 
+
+                        </FooterNetwork1>
+
+                        <FooterNetwork2>
+
+                             <FooterSpan2>Continually productize compelling dome packed with all Elated utilize website and creating  supply next-generation</FooterSpan2> 
+                        </FooterNetwork2>
+
+                        <FooterNetwork3>
+                            <FooterSpan3>
+                            Follow Us On:
+                            </FooterSpan3> <br />
+
+                            <FooterImg2 src={img1} alt="" />
+                            <FooterImg2 src={img2} alt="" />
+                            <FooterImg2 src={img3} alt="" />
+                            <FooterImg2 src={img4} alt="" />
+                            <FooterImg2 src={img5} alt="" />
+
+                        </FooterNetwork3>
+                    </FooterNetwork>
+
+                    <FooterType>
+                        <TypeHeader>Tour Type</TypeHeader>
+                        <TypeSpan1>Adventure Tours
+                                   Grouo Tour
+                                   Seasonal Tours
+                                   Relaxation Toure
+                                   Family Friendly Tour
+                        </TypeSpan1>
+
+                    </FooterType>
+
+                      <FooterContact>
+                           <ContactSpan>
+                                Contact info
+                          </ContactSpan>
+
+                          <ContactImges>
+
+                         <ContactImg src={img6} alt="" />
+                         <ContactNumber>88130-589-745-6987 +1655-456-523
+                        </ContactNumber>
+
+
+                          </ContactImges>
+
+                        
+                      </FooterContact>
+                  </FooterAdr>
+             </Wrapper2>
+       </Container2>
+
+
     </Container>
+
+    
+       
+       
+       
+      
+
+
   )
 }
 
--- React_tutorial/src/Figma/Footer/style.js
+++ React_tutorial/src/Figma/Footer/style.js
@@ -5,9 +5,6 @@ const Container = styled.div`
  
  width: 100%;
  height: 950px;
- padding: 0 130px;
- display: flex;
- align-items: center;
  background: #D9D9D9;
  margin-top: 70px
 
@@ -19,6 +16,9 @@ const Wrapper= styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 0 130px;
+  margin-top: 160px;
+  padding: 160px 160px
   `;
 
 const WrapperHeader= styled.div`
@@ -72,15 +72,147 @@ const WrapperHeader= styled.div`
   text-align: center;
   color: #FFFFFF;
   cursor: pointer;
+  `
 
-  
+  const Container2 = styled.div`
+ 
+ width: 100%;
+ height: 400px;
+ background: linear-gradient(0deg, #0C1218, #0C1218), url(3004-1024x685.jpg);
+`
+
+  const Wrapper2= styled.div`
+  width: 100%;
+  max-width: 1440px;
+  background: linear-gradient(0deg, #0C1218, #0C1218), url(3004-1024x685.jpg);
+  display: flex;
+  `;
+
+  const FooterAdr = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 130px;
   
   `
 
+  const FooterNetwork = styled.div`
+  margin-top: 70px;
+
+  
+
+  `
+  const FooterNetwork1 = styled.div`
+  
+  `
+
+  const FooterNetwork2 = styled.div`
+  width: 294px;
+  height: 75px;
+  margin: 24px 0px 34px 0px
+  
+  `
+  const FooterNetwork3 = styled.div`
+  
+  `
+
+  const FooterImg = styled.img`
+  
+  `
+
+  const FooterSpan = styled.span`
+  font-style: normal;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 28px;
+  color: #FFFFFF;
+  margin-left: 17px
+`
+
+const FooterSpan2 = styled.span`
+
+font-style: normal;
+font-weight: 500;
+font-size: 14px;
+line-height: 25px;
+color: #FFFFFF;
+ 
+`
+
+const FooterSpan3 = styled.span`
+   font-style: normal;
+   font-weight: 600;
+   font-size: 22px;
+   line-height: 33px;
+   color: #FFFFFF;
+`
+
+const FooterImg2 = styled.img`
+
+
+`
+
+const FooterType = styled.div`
+
+ margin-top: 70px;
+`
+const TypeHeader = styled.h1`
+
+`
+
+const TypeSpan1 = styled.h1`
+width: 164px;
+height: 187px;
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 40px;
+color: #FFFFFF;
+
+`
+
+const FooterContact = styled.div`
+
+`
+
+const ContactImges = styled.div`
+
+`
+
+const ContactSpan = styled.h1`
+   font-style: normal;
+   font-weight: 700;
+   font-size: 22px;
+   line-height: 30px;
+   color: #FFFFFF;
+
+
+`
+const ContactImg = styled.img`
+display: flex;
+
+`
+
+const ContactNumber = styled.h1`
+font-style: normal;
+font-weight: 600;
+font-size: 15px;
+line-height: 15px;
+color: #FFFFFF;
+width: 180px;
+height: 50px;
+
+
+
+`
 
 
 
 
 
 
-export {Container, BtnJoin, BtnEmail, FooterButton, Wrapper, FooterHeader, WrapperHeader}
+
+
+export {Container, ContactNumber, ContactImges, ContactImg, ContactSpan, FooterContact, TypeSpan1, TypeHeader, FooterType, FooterImg2, FooterSpan3, FooterNetwork1, FooterNetwork3, FooterNetwork2, FooterSpan,FooterSpan2, FooterImg, FooterNetwork,Container2, FooterAdr, Wrapper2, BtnJoin, BtnEmail, FooterButton, Wrapper, FooterHeader, WrapperHeader}
