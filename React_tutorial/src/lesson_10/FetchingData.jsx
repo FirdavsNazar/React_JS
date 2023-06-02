b924a09938bddcd96d34f5a0468cebdafe6f7b43
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-06-02 22:47:37 +0900

    Fetching Data

--- React_tutorial/src/lesson_10/FetchingData.jsx
+++ React_tutorial/src/lesson_10/FetchingData.jsx
@@ -3,21 +3,17 @@ import React, {useState, useEffect} from 'react'
 const FetchingData = () => {
 
     const [posts, setPosts] = useState([]);
+    const [ id, setId] = useState(1)
 
     useEffect(()=> {
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
         .then((res) => res.json())
         .then((response) => setPosts(response) )
-    }, [])
+    }, [id])
   return (
     <div>
-        {
-            posts.map((post) => {
-                return(
-                    <h2>{post.title}</h2>
-                )
-            })
-        }
+        <input type="text" value={id} onChange={(e)=> setId(e.target.value)} />
+        <h5>{posts.title}</h5>
     </div>
   )
 }
