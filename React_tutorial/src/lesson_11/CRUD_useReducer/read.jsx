c6467d6ea49bb3c1daeba5e16ff54a55bd9be0a3
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-06-10 00:11:44 +0900

    Crud_read(uR)

--- React_tutorial/src/lesson_11/CRUD_useReducer/read.jsx
+++ React_tutorial/src/lesson_11/CRUD_useReducer/read.jsx
@@ -34,6 +34,7 @@ const Read = () => {
                                 <td><button>Delete</button></td>
                             </tr>
                         )
+                        
                     })
                 }
             </tbody>
