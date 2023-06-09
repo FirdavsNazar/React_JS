fcddc3227184b56c0be52d5c119da92ce3219f8b
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-06-09 20:27:59 +0900

    useReducer

--- React_tutorial/src/lesson_10/Prop Drilling/App.jsx
+++ React_tutorial/src/lesson_10/Prop Drilling/App.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import A from '../Prop Drilling/A.jsx'
 
+
+
 const App = () => {
 
     const [name, setName] = useState('Firdavsbek')
---
+++ React_tutorial/src/lesson_11/useReducer.jsx
@@ -0,0 +1,7 @@
+import React, {useReducer} from 'react'
+
+export const useReducer = () => {
+  return (
+    <div>useReducer</div>
+  )
+}
