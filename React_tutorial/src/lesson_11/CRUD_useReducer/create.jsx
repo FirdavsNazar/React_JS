e8ae4036b1f288a701cade736bdb901364caf7a0
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-06-10 21:09:47 +0900

    create

--- React_tutorial/src/lesson_11/CRUD_useReducer/create.jsx
+++ React_tutorial/src/lesson_11/CRUD_useReducer/create.jsx
@@ -3,16 +3,24 @@ import {users} from './data.js'
 
 
 
-
 const fredReducer = (state,action)=>{
 
 switch(action.type) {
+    
+    //searching
     case 'FINDING': return users.filter((value) => value.name.toLowerCase().includes(action.payload.toLowerCase()))
+    //createing 
+    case 'Create' : return [...state, {
+        id: state.length + 1,
+        name: action.payload.ism
+    }]
+
 }
 }
 
 const Create = () => {
-    const [ name, setName] = useReducer('')
+    const [ name, setName] = useState('')
+    const [ title, setTitle] = useState('')
     const [state,dispatch] = useReducer(fredReducer,
         [
             {id: 1, name: 'Firdavs'},
@@ -25,6 +33,9 @@ const Create = () => {
   return (
     <div>
         <input onChange={(e)=> dispatch({type: 'FINDING', payload: e.target.value})} type="text" placeholder='Search...' />
+        <br />
+        <input onChange={(e)=> setTitle(e.target.value)} type="text" placeholder='Enter your name' />
+        <button onClick={()=> dispatch({type: 'Create', payload: {ism:title}})}>Create</button>
         <table width={'40%'} border='2' style={{borderCollapse:'collapse'}}>
             <thead>
                 <tr>
