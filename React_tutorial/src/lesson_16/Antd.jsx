4c687ad4b496945de65f6c1d9eb1f21465662db2
Nazarov Firdavs <fredrickuzb55@gmail.com>
2023-06-22 23:50:32 +0900

    Antd_MUI

--- React_tutorial/src/lesson_16/Antd.jsx
+++ React_tutorial/src/lesson_16/Antd.jsx
@@ -3,7 +3,7 @@ import {Button} from 'antd'
 
 const Antd = () => {
   return (
-    <div style={{display:'flex', flex:1, flexDirection:'column'}}>
+    <div style={{display:'flex', flex:1,  flexDirection:'column'}}>
         <h1>Antd</h1>
         <Button>Primary</Button>
     </div>
--- React_tutorial/src/lesson_16/Mui.jsx
+++ React_tutorial/src/lesson_16/Mui.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Button from '@mui/material/Button';
 
 const Mui = () => {
   return (
-    <div style={{display:'flex', flex:1, flexDirection:'column'}}>
+    <div style={{display:'flex', flex: 1, flexDirection:'column'}}>
         <h1>
             Mui
         </h1>
+        <Button>Primary</Button>
     </div>
   )
 }
